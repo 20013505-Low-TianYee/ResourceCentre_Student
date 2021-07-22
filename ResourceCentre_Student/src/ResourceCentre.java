e31151f90c3f93ac2ac0ae15c033ce13d182313f
20024783 <20024783@localhost>
2021-07-22 12:20:24 +0800

    Test

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -234,7 +234,7 @@ public class ResourceCentre {
 			}
 		}
 		return isReturned;
-		//
+		
 	}
 	public static void returnCamcorder(ArrayList<Camcorder> camcorderList) {
 		ResourceCentre.viewAllCamcorder(camcorderList);
