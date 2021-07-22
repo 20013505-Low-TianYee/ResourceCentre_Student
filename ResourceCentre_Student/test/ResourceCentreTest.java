4a2c74cd9faa24dc88fc4aa90240b0ab4d23b081
Umar <20028903@myrp.edu.sg>
2021-07-22 12:38:19 +0800

    Completed testAddChromebook() function

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -51,6 +51,7 @@ public class ResourceCentreTest {
 	@Test
 	public void testAddChromebook() {
 		//DONE BY: UMAR
+		//
 		// Item list is not null, so that can add a new item
 				assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
 				
