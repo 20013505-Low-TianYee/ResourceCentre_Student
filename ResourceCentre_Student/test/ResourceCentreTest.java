2485ac4acccaed6af18fb23460866c1483387414
20013505 <20013505@LAPTOP-1IUJLPET>
2021-08-04 23:40:52 +0800

    fixed

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -99,7 +99,7 @@ public class ResourceCentreTest {
 				
 				//test if the list of chromebook retrieved from the SourceCentre is empty
 						String allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
-						String testOutput = "";
+						String testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n", "ASSET TAG", "DESCRIPTION", "AVAILABLE", "DUE DATE", "OPERATING SYSTEM");
 						assertEquals("Check that ViewAllChromebookList", testOutput, allChromebook);
 						
 				//Given an empty list, after adding 2 items, test if the size of the list is 2
@@ -110,8 +110,8 @@ public class ResourceCentreTest {
 				//test if the expected output string same as the list of chromebook retrieved from the SourceCentre
 				allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
 
-				testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n","CB0011", "My Google Chromebook 1st", "Yes", "", "Mac OS");
-				testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n","CB0012", "SAMSUNG Chromebook 4+", "Yes", "", "Win 10");
+				testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n","CB0011", "My Google Chromebook 1st", "true", "", "Mac OS");
+				testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n","CB0012", "SAMSUNG Chromebook 4+", "true", "", "Win 10");
 
 				assertEquals("Check that ViewAllChromebookList", testOutput, allChromebook);
 	}
